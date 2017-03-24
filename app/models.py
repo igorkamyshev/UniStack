a884ee13c9663416fc75409aaf9b3e91584047a2
Igor Kamyshev <garik.novel@gmail.com>
2017-03-24 14:42:32 +0700

    US-000-7 Создать модель Экзамена

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Exam(models.Model):
+    name = models.CharField('название', max_length=255)
+
+    def __str__(self):
+        return self.name
