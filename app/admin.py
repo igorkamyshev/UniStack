b19f4f108e989b9bfc6639d8b0e0058b96c8c17c
Igor Kamyshev <garik.novel@gmail.com>
2017-03-24 23:27:12 +0700

    US-000-5 Создать модель Направления подготовки (цифры и название как в зачетках написано)

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Exam, Assistant, Country, Region, City
+from .models import Exam, Assistant, Country, Region, City, TrainingDirectionGroup, TrainingDirection
 
 admin.site.register(Exam)
+
 admin.site.register(Assistant)
+
 admin.site.register(Country)
 admin.site.register(Region)
 admin.site.register(City)
+
+admin.site.register(TrainingDirectionGroup)
+admin.site.register(TrainingDirection)
+
--- app/models.py
+++ app/models.py
@@ -62,3 +62,31 @@ class City(models.Model):
     class Meta:
         verbose_name = 'город'
         verbose_name_plural = 'города'
+
+
+class TrainingDirectionGroup(models.Model):
+    name = models.CharField('название', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'группа направлений подготовки'
+        verbose_name_plural = 'группы направлений подготовки'
+
+
+class TrainingDirection(models.Model):
+    name = models.CharField('название', max_length=255)
+    url = models.URLField()
+    code = models.CharField('код', max_length=255, default='0')
+    description = models.TextField('описание', null=True, blank=True)
+    intro = models.TextField('интро', null=True, blank=True)
+
+    group = models.ForeignKey(TrainingDirectionGroup, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'направление подготовки'
+        verbose_name_plural = 'направления подготовки'
