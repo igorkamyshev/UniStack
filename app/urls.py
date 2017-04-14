17b61618b3835767c4b7306965b5a0ded874b60e
Igor Kamyshev <garik.novel@gmail.com>
2017-04-14 20:35:21 +0700

    US-001-40 Сделать общий дизайн (классический)

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,12 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'app'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index')
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^about', views.AboutView.as_view(), name='about'),
+    url(r'^countries', views.CountryList.as_view(), name='country_list'),
+    url(r'^country/(?P<pk>[0-9]+)/$', views.CountryDetail.as_view(), name='country_detail'),
+    url(r'^region/(?P<pk>[0-9]+)/$', views.RegionDetail.as_view(), name='region_detail'),
+    url(r'^city/(?P<pk>[0-9]+)/$', views.CityDetail.as_view(), name='city_detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,7 +3,32 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
 
+from .models import Country, Region, City
+
 
 class IndexView(generic.TemplateView):
     template_name = 'app/index.html'
 
+
+class AboutView(generic.TemplateView):
+    template_name = 'app/index.html'
+
+
+class CountryList(generic.ListView):
+    model = Country
+    template_name = 'app/geography/country_list.html'
+
+
+class CountryDetail(generic.DetailView):
+    model = Country
+    template_name = 'app/geography/country_detail.html'
+
+
+class RegionDetail(generic.DetailView):
+    model = Region
+    template_name = 'app/geography/region_detail.html'
+
+
+class CityDetail(generic.DetailView):
+    model = City
+    template_name = 'app/geography/city_detail.html'
