ce5ff22c21ca8be11008f2713457d1079a209b7d
Igor Kamyshev <garik.novel@gmail.com>
2017-04-14 14:36:48 +0700

    US-001-41 Перейти на классиечкие Джанго-шаблоны

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+
+class IndexView(generic.TemplateView):
+    template_name = 'app/index.html'
+
--- unistack/urls.py
+++ unistack/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('front.urls')),
+    url(r'^', include('app.urls')),
+    url(r'^front_react', include('front.urls')),
     url(r'^api/v0/', include('api_v0.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^admin_tools/', include('admin_tools.urls')),
