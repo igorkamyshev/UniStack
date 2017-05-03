5f1514150ebb51ef7541d4fcba466ebbdba90ff5
Igor Kamyshev <garik.novel@gmail.com>
2017-05-03 23:27:00 +0700

    Edit settings.py for deploy (MySQL)

--- unistack/settings.py
+++ unistack/settings.py
@@ -90,10 +90,20 @@ WSGI_APPLICATION = 'unistack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'unistack',
+        'USER': 'TheGarik',
+        'PASSWORD': 'unistack1488',
+        'HOST': 'TheGarik.mysql.pythonanywhere-services.com'
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
