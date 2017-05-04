46a6af94147a0ca9c3a96916fce1484311cc6e4d
Igor Kamyshev <garik.novel@gmail.com>
2017-05-04 15:57:20 +0700

    minor fix for menu and code style

--- app/utils/grography.py
+++ app/utils/grography.py
@@ -2,11 +2,13 @@ import math
 
 
 def distance_in_km(lat1, lon1, lat2, lon2):
-    r = 6378.137  # Radius of earth in KM
+    R = 6378.137  # Radius of earth in KM
     d_lat = (lat2 * math.pi / 180) - (lat1 * math.pi / 180)
     d_lon = (lon2 * math.pi / 180) - (lon1 * math.pi / 180)
-    a = math.sin(d_lat / 2) * math.sin(d_lat / 2) + math.cos(lat1 * math.pi / 180) * math.cos(lat2 * math.pi / 180) * math.sin(d_lon / 2) * math.sin(d_lon / 2)
+    a = math.sin(d_lat / 2) * math.sin(d_lat / 2) + \
+        math.cos(lat1 * math.pi / 180) * math.cos(lat2 * math.pi / 180) * \
+        math.sin(d_lon / 2) * math.sin(d_lon / 2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    d = r * c
+    d = R * c
 
     return d
